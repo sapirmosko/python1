74cefb259e5a820b5699acf17548ac894e0e2763
sapirmosko <72347902+sapirmosko@users.noreply.github.com>
2022-12-05 19:33:55 +0200

    naming

--- browser.py
+++ browser.py
@@ -6,50 +6,50 @@ from requests_html import HTMLSession
 import threading
 
 
-inputFile = open("input/urls.input")
-outputDirName = "output"
+input_file = open("input/urls.input")
+output_folder = "output"
 session = HTMLSession()
 
 
 def main():
-    if not os.path.exists(outputDirName):
-        os.mkdir(outputDirName)
+    if not os.path.exists(output_folder):
+        os.mkdir(output_folder)
 
-    for index, urlPath in enumerate(inputFile, start=1):
+    for index, url_path in enumerate(input_file, start=1):
         print("Main    : create and start thread %d.", index)
         thread = threading.Thread(
-            target=createUrlFolder, args=(index, urlPath))
+            target=create_url_folder, args=(index, url_path))
         thread.start()
 
 
-def createUrlFolder(index, urlPath):
+def create_url_folder(index, url_path):
     if not os.path.exists("output/url" + str(index)):
         os.mkdir("output/url" + str(index))
 
-    jsonFileName = "output/url" + str(index) + "/browse.json"
-    pngFileName = "output/url" + str(index)
+    json_file_name = "output/url" + str(index) + "/browse.json"
+    png_file_name = "output/url" + str(index)
 
-    encodedScreenShot = getScreenShot(pngFileName, urlPath)
-    writeToJson(jsonFileName, urlPath, encodedScreenShot)
+    encoded_screen_shot = get_screen_shot(png_file_name, url_path)
+    write_to_json(json_file_name, url_path, encoded_screen_shot)
 
 
-def writeToJson(jsonFileName, urlPath, encodedScreenShot):
-    response = session.get(urlPath)
+def write_to_json(json_file_name, url_path, encoded_screen_shot):
+    response = session.get(url_path)
     links = list(response.html.links)
 
-    jsonFile = [{"html": response.text, "resources": links,
-                 "screenshot": encodedScreenShot}]
+    json_file = [{"html": response.text, "resources": links,
+                 "screenshot": encoded_screen_shot}]
 
-    with open(jsonFileName, 'w') as file:
-        json.dump(jsonFile, file, indent=4)
+    with open(json_file_name, 'w') as file:
+        json.dump(json_file, file, indent=4)
 
 
-def getScreenShot(pngFileName, urlPath):
-    hti = Html2Image(output_path=pngFileName)
-    hti.screenshot(url=urlPath, save_as="screenshot.png")
-    with open(pngFileName + "/screenshot.png", "rb") as img_file:
-        encodedScreenShot = base64.b64encode(img_file.read())
-    return str(encodedScreenShot)
+def get_screen_shot(png_file_name, url_path):
+    html_image = Html2Image(output_path=png_file_name)
+    html_image.screenshot(url=url_path, save_as="screenshot.png")
+    with open(png_file_name + "/screenshot.png", "rb") as img_file:
+        encoded_screen_shot = base64.b64encode(img_file.read())
+    return str(encoded_screen_shot)
 
 
 main()
