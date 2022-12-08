d8ba53bf7015746d4946de80acb517e7b0961e67
sapirmosko <72347902+sapirmosko@users.noreply.github.com>
2022-12-08 13:16:18 +0200

    after cr

--- browser.py
+++ browser.py
@@ -1,55 +1,52 @@
 import os
 import json
-from html2image import Html2Image
 import base64
+from html2image import Html2Image
 from requests_html import HTMLSession
-import threading
-
-
-input_file = open("input/urls.input")
-output_folder = "output"
-session = HTMLSession()
-
-
-def main():
-    if not os.path.exists(output_folder):
-        os.mkdir(output_folder)
-
-    for index, url_path in enumerate(input_file, start=1):
-        print("Main    : create and start thread %d.", index)
-        thread = threading.Thread(
-            target=create_url_folder, args=(index, url_path))
-        thread.start()
+from concurrent.futures import ThreadPoolExecutor
 
 
 def create_url_folder(index, url_path):
-    if not os.path.exists("output/url" + str(index)):
-        os.mkdir("output/url" + str(index))
+    path_url_dir = "output/url{}".format(str(index))
+    json_file_name = "{}/browse.json".format(path_url_dir)
 
-    json_file_name = "output/url" + str(index) + "/browse.json"
-    png_file_name = "output/url" + str(index)
-
-    encoded_screen_shot = get_screen_shot(png_file_name, url_path)
+    os.makedirs(path_url_dir, exist_ok=True)
+    encoded_screen_shot = get_screen_shot(path_url_dir, url_path)
     write_to_json(json_file_name, url_path, encoded_screen_shot)
 
 
 def write_to_json(json_file_name, url_path, encoded_screen_shot):
+    session = HTMLSession()
     response = session.get(url_path)
     links = list(response.html.links)
 
     json_file = [{"html": response.text, "resources": links,
                  "screenshot": encoded_screen_shot}]
 
-    with open(json_file_name, 'w') as file:
+    with open(os.path.realpath(json_file_name), 'w') as file:
         json.dump(json_file, file, indent=4)
 
 
-def get_screen_shot(png_file_name, url_path):
-    html_image = Html2Image(output_path=png_file_name)
+def get_screen_shot(path_url_dir, url_path):
+    html_image = Html2Image(output_path=path_url_dir)
+    png_file_path = "{}/screenshot.png".format(path_url_dir)
+
     html_image.screenshot(url=url_path, save_as="screenshot.png")
-    with open(png_file_name + "/screenshot.png", "rb") as img_file:
+    with open(os.path.realpath(png_file_path), "rb") as img_file:
         encoded_screen_shot = base64.b64encode(img_file.read())
+
     return str(encoded_screen_shot)
 
 
-main()
+def main():
+    os.makedirs("output", exist_ok=True)
+    input_file = open(os.path.realpath("input/urls.input"))
+
+    with ThreadPoolExecutor() as executor:
+        for index, url_path in enumerate(input_file, start=1):
+            print("create and start thread ", index)
+            executor.submit(create_url_folder, index, url_path)
+
+
+if __name__ == "__main__":
+    main()
